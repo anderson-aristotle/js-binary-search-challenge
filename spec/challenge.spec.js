1477185b0199ae39b5a6f48e7bc183c80a184fde
jordanallain <jallain@gmail.com>
2017-05-22 09:25:26 -0400

    Add starter code to challenge spec
    Include boiler plate test code in challenge spec

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+
+const expect = chai.expect
+
+const challenge = require('../lib/challenge')
+
+describe('Sync', function () {
+  it('is true', function () {
+    expect(challenge(true)).to.be.true
+  })
+})
+
+// Math.ceil(Math.log2(sortedArray.length))
+
+// https://git.generalassemb.ly/ga-wdi-boston/cs-algorithms#predicting-complexity
